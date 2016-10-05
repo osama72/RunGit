0ec836faed3e3fbf0662d43c097820784dedc939
aashgar <aashgar@yahoo.com>
2016-10-05 12:52:26 +0300

    Create ARTStudent.java

---
+++ src/Facade/ARTStudent.java
@@ -0,0 +1,14 @@
+package Facade;
+
+/**
+ *
+ * @author aashgar
+ */
+public class ARTStudent implements Student{
+
+    @Override
+    public void show() {
+        System.out.println("ART Student...");
+    }
+    
+}
